fb6fb441ed8260e0d3632d359ca89905e9257fc5
fxpw <fxpw@outlook.com>
2025-06-18 00:16:20 +0300

    up ver to 5.19

--- WeakAuras/Init.lua
+++ WeakAuras/Init.lua
@@ -9,8 +9,8 @@ WeakAuras.halfWidth = WeakAuras.normalWidth / 2
 WeakAuras.doubleWidth = WeakAuras.normalWidth * 2
 
 local versionStringFromToc = GetAddOnMetadata("WeakAuras", "Version")
-local versionString = "5.19.11"
-local buildTime = "2025".."04".."25".."19".."17".."00"
+local versionString = "5.19"
+local buildTime = "2025".."06".."18".."12".."00".."00"
 local isAwesomeEnabled = C_NamePlate and C_NamePlate.GetNamePlateForUnit and true or false
 
 local flavor
